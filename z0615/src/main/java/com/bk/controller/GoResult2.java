6b1b247bcd978b4c9acda067129dea1a0e9828ed
GITCT <GITCT@DESKTOP-QQDJ7QA>
2022-06-16 11:54:17 +0900

    유사도

--- z0615/src/main/java/com/bk/controller/GoResult2.java
+++ z0615/src/main/java/com/bk/controller/GoResult2.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/GoResult2")
 public class GoResult2 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		response.sendRedirect("Result.jsp");	
-	
-	}
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      try {
+         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+      response.sendRedirect("Result2.jsp");   
+   
+   }
 
 }
